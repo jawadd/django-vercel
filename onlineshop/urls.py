412a557342dc3349134ed9cd85bd2cb70b0b88bb
Jawad Hussain <jawadduet@gmail.com>
2024-05-10 16:29:18 +0500

    Add html template

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from onlineshop.views import OrderView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from . import views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -14,6 +15,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+     path('', views.index, name="index"),
     path('order/', OrderView.as_view(), name="Order"),
         path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
 ]

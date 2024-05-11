66a2797f5d56f6315695bff2aa20cf772ef8c78b
Jawad Hussain <jawadduet@gmail.com>
2024-05-11 17:29:54 +0500

    index.html 1st draft

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 echo "Building the project..."
 python3.12 -m pip install -r requirements.txt
-
-
-
 echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
